9ca9deeec794b8eff6820c09120651fcc108feff
juanlubh <juanlubh99@gmail.com>
2023-12-12 23:28:48 +0100

    trybbdd

--- index.php
+++ index.php
@@ -8,5 +8,17 @@
 
 <h1>Hola mundo</h1>
 
+<?php
+
+try {
+    $conexion = new PDO("mysql:host=localhost;dbname=amigos_peludos;charset=utf8", "root", "");
+    echo "Conectado";
+} catch (PDOException $e) {
+    echo "Error de Conectamos";
+    die("Error: " . $e->getMessage());
+}
+
+?>
+
 </body>
 </html>
